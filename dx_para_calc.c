471b10506521825d0fd58ea17c135d8045161a8c
mackenziestarr <mackestarr@gmail.com>
2014-03-24 18:49:08 -0400

    space space return

--- dx_para_calc.c
+++ dx_para_calc.c
@@ -1,6 +1,6 @@
-
+//lists dx7 operator similar parameter numbers for dev
+//takes one argument: the parameter number of the first operator
 #include <stdio.h>
-
 int main(int argc, char const *argv[])
 {
 	int i;
@@ -9,9 +9,5 @@ int main(int argc, char const *argv[])
 		para = atoi(argv[1]);
 		printf("OP%d: [%d]\n", i+1 , para + (21 * i));
 	}
-
-
-
-
 	return 0;
 }
